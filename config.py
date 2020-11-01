b8aa5d9a8db2682f987a0aac0be7e7bbb21bc5bf
Munge Ryan <mungeryan@gmail.com>
2020-11-01 16:02:52 +0300

    email configuring

--- config.py
+++ config.py
@@ -2,10 +2,27 @@ import os
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
+    SQLALCHEMY_TRACK_MODIFICATIONS = True
+    MAIL_PORT = 587
+    MAIL_SERVER = 'smtp.gmail.com'
+    MAIL_USE_TLS = True
+    MAIL_USE_SSL = False
+    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
+    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
+
 
 class ProdConfig(Config):
-    pass
+     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
 
 class DevConfig(Config):
-    pass
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://postgres:slowwhine@localhost/blogging'
+    DEBUG = True
+
+     
+config_options = {
+    'development':DevConfig,
+    'production':ProdConfig
+}
+    
 
