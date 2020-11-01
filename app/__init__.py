889603d968cbd9923d321ddd9b927f4d15bb10c3
Munge Ryan <mungeryan@gmail.com>
2020-11-01 15:28:08 +0300

    flask extensions

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,31 @@
 from flask import Flask
+from flask_login import login_manager
 from config import Config
 from flask_bootstrap import Bootstrap
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
+from flask_mail import Mail
+
+db = SQLAlchemy()
+mail = Mail()
+login_manager = LoginManager()
+login_manager.session_protection = 'strong'
+login_manager.login_view = 'auth.login'
 
 bootstrap = Bootstrap()
 
 def create_app():
     app = Flask(__name__)
-    app.config.from_object()
+    app.config.from_object(Config)
+    from .main import main as main_blueprint
+    from .auth import auth as authentication_blueprint
+
+    app.register_blueprint(authentication_blueprint)
+    app.register_blueprint(main_blueprint)
+
+    login_manager.init_app(app)
+    db.init_app(app)
+    bootstrap.init_app(app)
+    mail.init_app(app)
 
     return app
