7d09ebc8384c75f30fb88dbb526c8a0542737dc5
Munge Ryan <mungeryan@gmail.com>
2020-11-01 10:48:54 +0300

    creating class forms for login and reg

---
+++ app/auth/forms.py
@@ -0,0 +1,26 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField,SubmitField,ValidationError,PasswordField,BooleanField
+from wtforms.fields.simple import PasswordField
+from ..models import User
+from wtforms.validators import Required,Email,Length,EqualTo
+
+class LoginForm(FlaskForm):
+    username = StringField('Username',validators=[Required()])
+    password = PasswordField('Password',validators=[Required()])
+    remember = BooleanField('Remember Me!')
+    submit = SubmitField('Login')
+
+class RegistrationForm(FlaskForm):
+    username = StringField('Enter username', validators=[Required(),Length(min = 4,max = 20)])
+    email = StringField('Your email',validators=[Required(),Email()])
+    password = PasswordField('Password to use', validators=[Required(),EqualTo('password_confirm',message = 'Passwords need to match')])
+    password_confirm = PasswordField('Confirm entered password',validators=[Required()])
+    submit = SubmitField('Sign Up')
+
+    def validate_email(self,data_field):
+        if User.query.filter_by(email = data_field.data).first():
+            raise ValidationError(message = "Email already picked!!")
+
+    def validate_username(self, data_field):
+        if User.query.filter_by(username = data_field.data).first():
+            raise ValidationError(message = "Username already picked!!")
