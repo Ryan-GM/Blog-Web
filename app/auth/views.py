2bb6050b41a3d7c127a2497a5df8f51551ae6e8b
Munge Ryan <mungeryan@gmail.com>
2020-11-02 14:46:59 +0300

    rendering template to login file

--- app/auth/views.py
+++ app/auth/views.py
@@ -1,30 +1,30 @@
-from flask_login import login_user,login_required,logout_user
-from flask import render_template,flash,url_for,redirect,request
+from flask import render_template,flash, request, redirect, url_for
+from flask_login import login_user, logout_user,login_required
 from app.auth import auth
-from .forms import RegistrationForm,LoginForm
-from ..email import mail_message
 from app.models import User
+from .forms import RegForm,LoginForm
+from ..email import mail_message
 
 @auth.route('/login',methods = ['POST','GET'])
 def login():
     form = LoginForm()
     if form.validate_on_submit():
         user = User.query.filter_by(username = form.username.data).first()
-        if user != None and user.verify_password(form.password):
+        if user != None and user.verify_password(form.password.data):
             login_user(user,form.remember.data)
             return redirect(request.args.get('next') or url_for('main.index'))
-        flash('Wrong Credentials(check password/username)')
+        flash('Wrong Username or Password')
     return render_template('auth/login.html',form = form)
 
 @auth.route('/signup',methods = ["POST","GET"])
 def signup():
-    form = RegistrationForm()
+    form = RegForm()
     if form.validate_on_submit():
-        user = User(username = form.usernam.data,email = form.email.data,password = form.password.data)
+        user = User(username=form.username.data, email = form.email.data, password=form.password.data)
         user.save()
-        mail_message("Welcome to Blog-Web","email/welcome",user.email,user = user)
-        return redirect(url_for('auth.login'))
-    return render_template('auth/signup.html',regform = form)
+        mail_message("Welcome to D-Blog","email/welcome",user.email,user=user)
+        return  redirect(url_for('auth.login'))
+    return render_template('auth/signup.html',registration_form=form )
 
 @auth.route('/logout')
 @login_required
